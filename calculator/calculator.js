481b502903f0552312a307732a052a03e86e16e0
azuliat92 <zuwoyemi2@gmail.com>
2022-08-30 18:12:30 +0100

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,114 @@
+var del = document.getElementById('btn-del');
+del.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = '';
+}
+
+var btn1 = document.getElementById('btn-1');
+btn1.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn1.value;
+}
+
+
+var btn2 = document.getElementById('btn-2');
+btn2.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn2.value;
+}
+
+var btn3 = document.getElementById('btn-3');
+btn3.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn3.value; 
+}
+
+var btn4 = document.getElementById('btn-4');
+btn4.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn4.value;
+}
+
+var btn5 = document.getElementById('btn-5');
+btn5.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn5.value;
+}
+
+var btn6 = document.getElementById('btn-6');
+btn6.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn6.value;
+}
+
+var btn7 = document.getElementById('btn-7');
+btn7.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn7.value;
+}
+
+var btn8 = document.getElementById('btn-8');
+btn8.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn8.value;
+}
+
+var btn9 = document.getElementById('btn-9');
+btn9.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn9.value;
+}
+
+var btn0 = document.getElementById('btn-0');
+btn0.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn0.value;
+}
+
+var btn_plus = document.getElementById('btn+');
+btn_plus.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_plus.value;
+}
+
+var btn_minus = document.getElementById('btn-');
+btn_minus.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_minus.value;
+}
+
+var btn_div = document.getElementById('btn/');
+btn_div.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_div.value;
+}
+
+var btn_mul = document.getElementById('btn*');
+btn_mul.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_mul.value;
+}
+
+var btn_log = document.getElementById('btn-log');
+btn_log.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_log.value + '(';
+}
+
+var btn_cos = document.getElementById('btn-cos');
+btn_cos.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_cos.value + '(';
+}
+
+var btn_tan = document.getElementById('btn-tan');
+btn_tan.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_tan.value + '(';
+}
+
+var btn_mod = document.getElementById('btn-mod');
+btn_mod.onclick = function () {
+    var screen = document.getElementById('screen');
+    screen.value = screen.value + btn_mod.value + '(';
+}
